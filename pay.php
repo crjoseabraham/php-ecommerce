5dcbbd9dfe4fb5622837c93b29ffe64f97c91200
Jose Abraham C <abrahamcr3@gmail.com>
2017-10-14 21:08:36 -0400

    Update pay.php

--- pay.php
+++ pay.php
@@ -1,16 +1,7 @@
 <?php
-session_start();
-require_once 'dbconnection.php';
+require_once 'operations.php';
 
-$total = $_POST['total_amount'];
-$_SESSION['cash'] -= $total;
-
-$delete = "DELETE FROM details";
-$conn->query($delete);
-if ($conn->query($delete) === TRUE) {    
-} else {
-    echo "Error: " . $delete . "<br>" . $conn->error;
-}
-
-$conn->close();
+$ship = $_POST['ship'];
+$operation = new Operations();
+$pay = $operation->ProcessPayment($ship);
 ?>
