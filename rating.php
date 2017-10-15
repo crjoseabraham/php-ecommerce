18ee92d20cd74b4c3457a3f4a7602b4f900f8413
Jose Abraham C <abrahamcr3@gmail.com>
2017-10-14 21:08:56 -0400

    Update rating.php

--- rating.php
+++ rating.php
@@ -1,32 +1,10 @@
 <?php
-session_start();
-require_once 'dbconnection.php';
+require_once 'operations.php';
 
 $sessionid = session_id();
 $rate_value = $_POST['rate'];
-$itemid = $_POST['item_id'];
+$item_id = $_POST['item_id'];
 
-$query1 = "SELECT id FROM tbl_rating WHERE sessionid='$sessionid' AND productid='$itemid'";
-$result = $conn->query($query1);
-$row = $result->fetch_assoc();
-if ($result->num_rows > 0) {
-    echo "<script> alert('You already rated this item. You can only vote once per session'); </script>";
-    $votes = $conn->query("SELECT COUNT(*) as `total` FROM tbl_rating WHERE productid='$itemid'");
-    $totalvotes = $votes->fetch_assoc();
-    $avg = $conn->query("SELECT AVG(`rate`) as `average` FROM tbl_rating WHERE productid='$itemid'");
-    $avg_result = $avg->fetch_assoc();
-    echo "Rating: ".number_format((float)$avg_result['average'],1,'.','')."/5 ";
-    echo "(of ".$totalvotes['total']." votes)";
-} else {
-    $sql = "INSERT INTO tbl_rating (productid, rate, sessionid) VALUES ('$itemid','$rate_value','$sessionid')";
-    if ($conn->query($sql) === TRUE) {
-        $votes = $conn->query("SELECT COUNT(*) as `total` FROM tbl_rating WHERE productid='$itemid'");
-        $totalvotes = $votes->fetch_assoc();
-        $avg = $conn->query("SELECT AVG(`rate`) as `average` FROM tbl_rating WHERE productid='$itemid'");
-        $avg_result = $avg->fetch_assoc();
-        echo "Rating: ".number_format((float)$avg_result['average'],1,'.','')."/5 ";
-        echo "(of ".$totalvotes['total']." votes)";
-    } else { echo mysqli_error($conn); }
-}
-$conn->close();
+$operation = new Operations();
+$rate = $operation->Rate($sessionid, $item_id, $rate_value);
 ?>
