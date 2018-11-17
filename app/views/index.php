6dfb6eee9a12f9488e94ca7b97519c8a38ac8845
Jose Abraham Castillo <crjoseabraham@gmail.com>
2018-11-17 00:13:49 -0400

    UI improvements to display the products better

--- app/views/index.php
+++ app/views/index.php
@@ -1,12 +1,29 @@
-<?php 
-echo "<h1> Home page </h1><br><br>";
-foreach ($data as $item) {
-	echo " <ul>
-	<li> ID: {$item['product_id']} </li>
-	<li> Description: {$item['description']} </li>
-	<li> Price: {$item['price']} </li>
-	<li> Picture: {$item['picture']} </li>
-	<li> Rating: {$item['rating']} </li>
-	</ul>";
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP MVC Shopping cart</title>
+	<link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+	<h1> Home Page </h1>
+	<hr>
+	<div class="products-container">
+	<?php foreach ($data as $item) : ?>
+	<div class="product-card">
+		<img src="<?= $item['picture']; ?>" alt="<?= $item['description']; ?>" class="product-card__img">
+		<hr>
+		<div class="product-card__product-details">
+			<p class="product-card__product-name"> <?= $item['description']; ?> </p>
+			<span class="product-card__product-price"> $<?= $item['price']; ?> </span>
+		</div>
+		<form action="" class="product-card__form">
+			<input type="number" class="product-card__quantity" name="quantity">
+			<button type="submit"> Add </button>
+		</form>
+		<p class="product-card__rating"> Rating: <?= $item['rating']; ?> </p>
+	</div>
+	<?php endforeach; ?>
+	</div>
+</body>
+</html>
