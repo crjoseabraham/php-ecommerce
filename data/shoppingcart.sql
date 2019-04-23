093e1553e9aa44199872a348e9127e0967f2c4aa
Jose Abraham Castillo <crjoseabraham@gmail.com>
2019-04-23 17:13:16 -0400

    Update .sql file for 'Remember login' function

--- data/shoppingcart.sql
+++ data/shoppingcart.sql
@@ -1,150 +1,309 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.37-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.5.0.5196
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 23-04-2019 a las 23:11:49
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Volcando estructura de base de datos para shoppingcart
-CREATE DATABASE IF NOT EXISTS `shoppingcart` /*!40100 DEFAULT CHARACTER SET latin1 */;
+--
+-- Base de datos: `shoppingcart`
+--
+CREATE DATABASE IF NOT EXISTS `shoppingcart` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `shoppingcart`;
 
--- Volcando estructura para tabla shoppingcart.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(60) NOT NULL,
-  `name` varchar(60) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
--- Volcando datos para la tabla shoppingcart.user: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `email`, `name`, `password`) VALUES
-	(37, 'jose@gmail.com', 'Jose', '$2y$10$acOeRYGELTRKtCcF/M/QGOXgJ4S8FazjS.9GgtdviCIg4/NjpjA8C');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+--
+-- Estructura de tabla para la tabla `cart`
+--
 
--- Volcando estructura para tabla shoppingcart.product
-CREATE TABLE IF NOT EXISTS `product` (
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(30) NOT NULL,
-  `price` float NOT NULL,
-  `picture` varchar(255) NOT NULL,
-  `rating` float NOT NULL,
-  PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla shoppingcart.product: ~7 rows (aproximadamente)
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` (`product_id`, `description`, `price`, `picture`, `rating`) VALUES
-	(1, 'Apple', 0.3, 'img/apples.jpg', 2.5),
-	(2, 'Beer', 2, 'img/beer.jpg', 2.5),
-	(3, 'Water', 1, 'img/water.jpg', 1),
-	(4, 'Cheese', 3.74, 'img/cheese.jpg', 1),
-	(5, 'Burger', 5.99, 'img/hamburger.jpg', 0),
-	(6, 'Taco', 3.99, 'img/taco.jpg', 4),
-	(7, 'Orange Juice', 2.35, 'img/orange-juice.jpg', 3.5);
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-
--- Volcando estructura para tabla shoppingcart.cart
-CREATE TABLE IF NOT EXISTS `cart` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `status` bit(1) NOT NULL DEFAULT b'1',
-  PRIMARY KEY (`id`),
-  KEY `FK_cart_user` (`user_id`),
-  CONSTRAINT `FK_cart_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `status` bit(1) NOT NULL DEFAULT b'1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla shoppingcart.cart: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla shoppingcart.cart_items
-CREATE TABLE IF NOT EXISTS `cart_items` (
+--
+-- Estructura de tabla para la tabla `cart_items`
+--
+
+CREATE TABLE `cart_items` (
   `cart_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `subtotal` float NOT NULL,
-  KEY `FK_cart_items_product` (`product_id`),
-  KEY `FK_cart_items_cart` (`cart_id`),
-  CONSTRAINT `FK_cart_items_cart` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_cart_items_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `subtotal` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla shoppingcart.cart_items: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `cart_items` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_items` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla shoppingcart.order
-CREATE TABLE IF NOT EXISTS `order` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Estructura de tabla para la tabla `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `transport_costs` int(11) NOT NULL,
-  `total` float NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `FK_order_user` (`user_id`),
-  CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `total` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla shoppingcart.order: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla shoppingcart.order_details
-CREATE TABLE IF NOT EXISTS `order_details` (
+--
+-- Estructura de tabla para la tabla `order_details`
+--
+
+CREATE TABLE `order_details` (
   `order_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `subtotal` float DEFAULT NULL,
-  KEY `FK_order_items_product` (`product_id`),
-  KEY `FK_order_items_order` (`order_id`),
-  CONSTRAINT `FK_order_items_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_order_items_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `subtotal` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla shoppingcart.order_details: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla shoppingcart.rating
-CREATE TABLE IF NOT EXISTS `rating` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rating_product_id` int(11) NOT NULL,
-  `rating_value` float NOT NULL,
-  `session_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_rating_product` (`rating_product_id`),
-  CONSTRAINT `FK_rating_product` FOREIGN KEY (`rating_product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
+--
+-- Estructura de tabla para la tabla `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `description` varchar(30) NOT NULL,
+  `price` float NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `rating` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla shoppingcart.rating: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+--
+-- Volcado de datos para la tabla `product`
+--
 
--- Volcando estructura para tabla shoppingcart.session
-CREATE TABLE IF NOT EXISTS `session` (
+INSERT INTO `product` (`product_id`, `description`, `price`, `picture`, `rating`) VALUES
+(1, 'Apple', 0.3, 'img/apples.jpg', 2.5),
+(2, 'Beer', 2, 'img/beer.jpg', 2.5),
+(3, 'Water', 1, 'img/water.jpg', 1),
+(4, 'Cheese', 3.74, 'img/cheese.jpg', 1),
+(5, 'Burger', 5.99, 'img/hamburger.jpg', 0),
+(6, 'Taco', 3.99, 'img/taco.jpg', 4),
+(7, 'Orange Juice', 2.35, 'img/orange-juice.jpg', 3.5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `rating_product_id` int(11) NOT NULL,
+  `rating_value` float NOT NULL,
+  `session_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `remembered_logins`
+--
+
+CREATE TABLE `remembered_logins` (
+  `token_hash` varchar(64) NOT NULL,
   `session_id` varchar(50) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `start` datetime NOT NULL,
-  `end` datetime DEFAULT NULL,
-  `status` bit(1) NOT NULL DEFAULT b'1',
-  PRIMARY KEY (`session_id`),
-  KEY `FK_session_user` (`user_id`),
-  CONSTRAINT `FK_session_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `expires_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla shoppingcart.session: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `session` DISABLE KEYS */;
-/*!40000 ALTER TABLE `session` ENABLE KEYS */;
+-- --------------------------------------------------------
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Estructura de tabla para la tabla `session`
+--
+
+CREATE TABLE `session` (
+  `session_id` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cash` float NOT NULL DEFAULT '100',
+  `start` datetime NOT NULL,
+  `end` datetime DEFAULT NULL,
+  `status` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_cart_user` (`user_id`);
+
+--
+-- Indices de la tabla `cart_items`
+--
+ALTER TABLE `cart_items`
+  ADD KEY `FK_cart_items_product` (`product_id`),
+  ADD KEY `FK_cart_items_cart` (`cart_id`);
+
+--
+-- Indices de la tabla `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `FK_order_user` (`user_id`);
+
+--
+-- Indices de la tabla `order_details`
+--
+ALTER TABLE `order_details`
+  ADD KEY `FK_order_items_product` (`product_id`),
+  ADD KEY `FK_order_items_order` (`order_id`);
+
+--
+-- Indices de la tabla `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indices de la tabla `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_rating_product` (`rating_product_id`);
+
+--
+-- Indices de la tabla `remembered_logins`
+--
+ALTER TABLE `remembered_logins`
+  ADD PRIMARY KEY (`token_hash`),
+  ADD KEY `session_id` (`session_id`);
+
+--
+-- Indices de la tabla `session`
+--
+ALTER TABLE `session`
+  ADD PRIMARY KEY (`session_id`),
+  ADD KEY `FK_session_user` (`user_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `FK_cart_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cart_items`
+--
+ALTER TABLE `cart_items`
+  ADD CONSTRAINT `FK_cart_items_cart` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_cart_items_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `FK_order_items_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_order_items_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `FK_rating_product` FOREIGN KEY (`rating_product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `remembered_logins`
+--
+ALTER TABLE `remembered_logins`
+  ADD CONSTRAINT `remembered_logins_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `session` (`session_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `session`
+--
+ALTER TABLE `session`
+  ADD CONSTRAINT `FK_session_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
