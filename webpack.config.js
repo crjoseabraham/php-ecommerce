9d7afa83bb18d67e769f1b92b71b3f28337028d5
Jose Abraham Castillo <crjoseabraham@gmail.com>
2020-03-10 22:31:04 -0400

    Project setup + basic structure markup

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: {
-    app: [
-      '@babel/polyfill',
-      './assets/javascript/index.js'
-    ]
-  },
-  output: {
-    filename: 'app.bundle.js',
-    path: path.resolve(__dirname, 'public/js')
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: [/node_modules/, /vendor/],
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }]
-  }
-}
+    entry: "./resources/scripts/index.js",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "public", "assets", "scripts")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/, /vendor/],
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
+};
