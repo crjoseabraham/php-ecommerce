f8e258e7755f948fc55008057b524ca2cba5268e
Jose Abraham Castillo <crjoseabraham@gmail.com>
2020-06-30 17:56:27 -0400

    Progress on item details modal

--- resources/views/cache/27f3eb99fed362cbd85ac92318f44efb4aaa4691.php
+++ resources/views/cache/27f3eb99fed362cbd85ac92318f44efb4aaa4691.php
@@ -1,6 +1,10 @@
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-<br><br>
-<?php echo e($product_id); ?>
-
-<br><br>
-<?php echo e($description); ?><?php /**PATH C:\xampp\htdocs\shoppingcart\resources\views/components/item_details.blade.php ENDPATH**/ ?>
+<div class="item-details">
+    <div class="item-details__picture">
+        <img src="./img/product/<?php echo e($product_id); ?>.jpg" alt="<?php echo e($description); ?>">
+    </div>
+    <div class="item-details__info">
+        <h3 class="sans fs16"><?php echo e($description); ?></h3>
+        <h4 class="sans fs18 mt-2">$<?php echo e($price); ?></h4>
+        <button class="btn btn--primary"> Add to cart</button>
+    </div>
+</div><?php /**PATH C:\xampp\htdocs\shoppingcart\resources\views/components/item_details.blade.php ENDPATH**/ ?>
--- resources/views/components/item_details.blade.php
+++ resources/views/components/item_details.blade.php
@@ -1,5 +1,10 @@
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-<br><br>
-{{$product_id}}
-<br><br>
-{{$description}}
+<div class="item-details">
+    <div class="item-details__picture">
+        <img src="./img/product/{{ $product_id }}.jpg" alt="{{ $description }}">
+    </div>
+    <div class="item-details__info">
+        <h3 class="sans fs16">{{ $description }}</h3>
+        <h4 class="sans fs18 mt-2">${{ $price }}</h4>
+        <button class="btn btn--primary"> Add to cart</button>
+    </div>
+</div>
